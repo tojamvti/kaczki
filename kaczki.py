a7367a6cc1112a0fe25ce1870a2e77ab5449639e
Mateusz Szygulski <51803500+tojamvti@users.noreply.github.com>
2023-03-29 22:36:39 +0200

    Update kaczki.py

--- kaczki.py
+++ kaczki.py
@@ -1,34 +1,34 @@
 import openpyxl
-from openpyxl import load_workbook
 
 def ducks():
-    
     heights = []
     widths = []
     
     wb = openpyxl.load_workbook('kaczki.xlsx')
-    ws = wb['Arkusz1'] 
-    rows = ws.iter_rows(min_row=1,max_row=21,min_col=1,max_col=2) 
+    ws = wb['Arkusz1']
+    rows = ws.iter_rows(min_row=1, max_row=21, min_col=1, max_col=2)
 
-    for a,b in rows:
+    for a, b in rows:
         heights.append(a.value)
         widths.append(b.value)
+
+    MAXIMUM_WIDTH = 50
     
-    MAXIMUM_WIDTH = widths[0]
+    chickens = list(zip(heights[1:], widths[1:]))
+    sorted_chickens = sorted(chickens, key=lambda x: x[0], reverse=True)
     
-    heights.pop(0)
-    heights.sort(reverse=True)
+    total_height = 0
+    total_width = 0
+    count = 0
     
-    widths.pop(0)
-    widths.sort(reverse=True)
-   
-    for i in heights:
-        rediheight = 0
-        rediwidth = 0
-        while rediwidth <= MAXIMUM_WIDTH:
-            rediheight += heights[i]
-            rediwidth += widths[i]
-            
-    return f"maksymalna dostępna suma: {rediheight}"
+    for chicken in sorted_chickens:
+        if total_width + chicken[1] <= MAXIMUM_WIDTH:
+            total_height += chicken[0]
+            total_width += chicken[1]
+            count += 1
+        else:
+            break
+
+    print(f"Maximum sum of heights of {count} chickens is {total_height}")
 
 print(ducks())
