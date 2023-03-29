616b56b1e384c5eb9004191a98085f0fd29aef41
Mateusz Szygulski <51803500+tojamvti@users.noreply.github.com>
2023-03-29 13:41:36 +0200

    Update kaczka.py

--- kaczka.py
+++ kaczka.py
@@ -6,7 +6,7 @@ def ducks():
     heights = []
     widths = []
     
-    wb = openpyxl.load_workbook('zadanie-rekrutacyjne.xlsx')
+    wb = openpyxl.load_workbook('kaczki.xlsx')
     ws = wb['Arkusz1'] 
     rows = ws.iter_rows(min_row=1,max_row=21,min_col=1,max_col=2) 
 
